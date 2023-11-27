85559e3d523efb4ee0619e9ff5237f13d3806859
Laurant_NG <2496803478@qq.com>
2023-11-27 16:14:37 +0800

    feat: comment the code for comprehension

--- models/loss.py
+++ models/loss.py
@@ -6,15 +6,29 @@ import torchvision
 import numpy as np
 
 def range_compressor(hdr_img, mu=5000):
+    """
+    @brief: 对HDR进行范围压缩
+    @param: hdr_img 输入的HDR张量
+    @param: mu 参数，默认5000
+    @return: 压缩到标准范围内的图像
+    @note: 分子使用torch.log(),因为hdr_img是张量
+           分母使用math.log(),因为mu是标量
+    """
     return (torch.log(1 + mu * hdr_img)) / math.log(1 + mu)
 
 
 class L1MuLoss(nn.Module):
+    """
+    @brief: 计算L1损失，构成最终的损失函数
+    """
     def __init__(self, mu=5000):
         super(L1MuLoss, self).__init__()
         self.mu = mu
 
     def forward(self, pred, label):
+        """
+        @brief: 重写forward方法，自定义损失函数的计算过程
+        """
         mu_pred = range_compressor(pred, self.mu)
         mu_label = range_compressor(label, self.mu)
         return nn.L1Loss()(mu_pred, mu_label)
@@ -28,24 +42,32 @@ class VGGPerceptualLoss(nn.Module):
         blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
         blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
         blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
+        # 设置传播的过程中不计算梯度，保持模型的提取能力，不对权重进行微调
         for bl in blocks:
             for p in bl:
                 p.requires_grad = False
+
         self.blocks = torch.nn.ModuleList(blocks)
-        self.transform = torch.nn.functional.interpolate
+        self.transform = torch.nn.functional.interpolate # 对图像进行插值操作
         self.resize = resize
-        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+
+        # 将均值和标准差存入缓冲区，方便对图像进行归一化操作
+        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1)) 
         self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
 
     def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
+        # 若图像的通道数不是3，则将其复制为3通道的图像
         if input.shape[1] != 3:
             input = input.repeat(1, 3, 1, 1)
             target = target.repeat(1, 3, 1, 1)
+        # 归一化
         input = (input-self.mean) / self.std
         target = (target-self.mean) / self.std
+
         if self.resize:
             input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
             target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        
         loss = 0.0
         x = input
         y = target
